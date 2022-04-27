499b4faaed256eae10d6fa47ff2afc99e21e6163
jin3014test1 <98628465+jin3014test1@users.noreply.github.com>
2022-04-27 16:37:02 +0800

    Update mt7621.mk

--- 8820v2/mt7621.mk
+++ 8820v2/mt7621.mk
@@ -1848,11 +1848,9 @@ TARGET_DEVICES += zte_e8820s
 
 define Device/zte_e8820v2
   $(Device/dsa-migration)
-  $(Device/uimage-lzma-loader)
   IMAGE_SIZE := 16064k
   DEVICE_VENDOR := ZTE
   DEVICE_MODEL := E8820V2
-  DEVICE_COMPAT_VERSION := 1.1
   DEVICE_PACKAGES := \
 	kmod-mt7603 kmod-mt76x2 kmod-usb3 kmod-usb-ledtrig-usbport wpad luci 
 endef
